07b01629aa7eea4611696f8c8cad034a428cd750
kodhanjo <felix.osambo@student.moringaschool.com>
2021-08-25 23:37:51 +0300

    registered auth

---
+++ makefile
@@ -0,0 +1,2 @@
+python3.8 manage.py db migrate -m "Migration"
+python3.6 manage.py db upgrade
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager,Server
-# from  flask_migrate import Migrate, MigrateCommand
+from  flask_migrate import Migrate, MigrateCommand
 from app.models import User
 
 
@@ -12,8 +12,8 @@ app=create_app('developement')
 manager=Manager(app)
 manager.add_command('server',Server)
 
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 
 #use the manager shell decorator to access the shell on the command line
